4086130074e8f6a714e7e6e098fcf3393eac8af2
Ruel Nopal <radon1284@yahoo.com>
2016-11-13 02:51:08 +0800

    hcihjwdgjsc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 	def pldtua_logo
     	image_tag('logo.png', :class => "img-responsive")
   	end
+
+  	def pldtua_logo_small
+    	image_tag('logo-small.png', :class => "img-responsive")
+  	end
 end
